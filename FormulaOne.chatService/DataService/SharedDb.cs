26b720ba2824abbb52d134ed080d3c17d9b81476
hiep <nguyendinhhiep317@gmail.com>
2024-05-20 21:26:58 +0700

    create-asp

---
+++ FormulaOne.chatService/DataService/SharedDb.cs
@@ -0,0 +1,12 @@
+﻿using FormulaOne.chatService.Models;
+using System.Collections.Concurrent;
+using System.Threading.RateLimiting;
+
+namespace FormulaOne.chatService.DataService
+{
+    public class SharedDb
+    {
+        private readonly ConcurrentDictionary<string, UserConnection> _connections = new();
+        public ConcurrentDictionary<string, UserConnection> connections => _connections;
+    }
+}
---
+++ FormulaOne.chatService/Models/UserConnection.cs
@@ -0,0 +1,9 @@
+﻿namespace FormulaOne.chatService.Models
+{
+    public class UserConnection
+    {
+        public string Username { get; set; } = string.Empty;
+        public string ChatRoom { get; set; } = string.Empty;
+
+    }
+}
